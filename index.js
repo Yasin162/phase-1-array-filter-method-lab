f225d7bdaa52b6cb60c17db2294abae7d4c998b7
Yasin <yasinabdussaburr@gmail.com>
2021-09-08 21:14:12 -0400

    commit tree 9a845facca71bb81263c70d40f2726be8c052b41

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// function findMatching(name, drivers) {return drivers }
+
+
+function findMatching(drivers, name) {
+    if (drivers = name) {
+        return `'${drivers}', '${name}'`}
+    // return `${drivers}, = ${name}`;
+}
